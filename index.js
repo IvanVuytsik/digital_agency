8de4968fcf709b4f4829b22288357488ed0112fd
Ivan Vuytsik <vuicik1989@gmail.com>
2022-09-15 12:10:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+window.addEventListener("scroll", () => {
+    document.querySelector('nav').classList.toggle('window-scrolled', 
+    window.scrollY > 0);
+})
+
+
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 15}deg)">${character}</span>`).join('');
+})
+
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        bulletClass: "swiper-pagination-bullet",
+    },
+
+    breakpoints: {
+        0: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+            },
+
+        599: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+        1023: {
+                slidesPerView: 3,
+                spaceBetween: 60, 
+        },
+    }
+
+});
+   
+
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = "none";
+    closeNavBtn.style.display = "inline-block";
+}
+
+const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = "inline-block";
+    closeNavBtn.style.display = "none";
+}
+
+openNavBtn.addEventListener('click', openNav)
+closeNavBtn.addEventListener('click', closeNav)
+
+if(document.body.clientWidth < 1024){
+    nav.querySelectorAll('li a').forEach(navLink => {
+        navLink.addEventListener('click', closeNav)
+    })
+}
